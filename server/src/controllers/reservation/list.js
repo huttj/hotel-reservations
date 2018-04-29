4d4712a3bd22a161384a2725e8f2af6a98b1ecdd
Joshua <joshua@huttj.com>
2018-04-29 16:23:25 -0700

    Fix REST list method

--- server/src/controllers/reservation/list.js
+++ server/src/controllers/reservation/list.js
@@ -1,11 +1,23 @@
+import pick from 'lodash/pick';
 import { Reservation } from '../../models';
 
-export default async function findReservationById(req, res, next) {
+export default async function list(req, res, next) {
   try {
 
-    const { hotelName, arrivalDate, departureDate } = req.query;
+    const query = pick(req.query, ['name', 'hotelName', 'arrivalDate', 'departureDate']);
 
-    const results = await Reservation.find({ hotelName, arrivalDate, departureDate });
+    if (query.name) {
+      query.name = {
+        $regex: query.name, $options: 'i'
+      }
+    }
+    if (query.hotelName) {
+      query.hotelName = {
+        $regex: query.hotelName, $options: 'i'
+      }
+    }
+
+    const results = await Reservation.find(query);
 
     res.send(results);
 
